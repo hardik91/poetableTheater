d414b4be0ced2735c4ef693b1bde7fdec0e283fd
Hardik Mistry <hardik.mistry@outlook.com>
2015-08-24 14:53:31 -0500

    final changes

--- TimeSheetHack.js
+++ TimeSheetHack.js
@@ -6,13 +6,16 @@ for(var i = 2;i < 7 ; i++){
   var widget = iBody.find('.widgetwrapper')[i];
   var dayId = $(widget).attr('id').split("_").pop();
   var matchRateId = "widget_day_"+dayId+"_ddlmatchrate";
-  iBody.find("input#"+matchRateId).attr('value','Regular Pay');
+  iBody.find("input#"+matchRateId).addClass('valid');
+    iBody.find("input#"+matchRateId).val('Regular Pay');
+
+  $(iBody.find('input#'+matchRateId).siblings()[1]).html('4346950');
   iBody.find("div#wrapper_widget_day_"+dayId).find('tr#trStartEnd').show();
   iBody.find("div#wrapper_widget_day_"+dayId).find('tr#trBreak').show();
-  iBody.find("input#widget_day_"+dayId+"_starttime").timepicker('setTime','9:00')
-  iBody.find("input#widget_day_"+dayId+"_starttime").val('9:00');
-  iBody.find("input#widget_day_"+dayId+"_starttime").timepicker('setTime','17:30')
-  iBody.find("input#widget_day_"+dayId+"_endtime").val('17:30');
+  iBody.find("input#widget_day_"+dayId+"_starttime").timepicker('setTime','9:00 AM')
+  iBody.find("input#widget_day_"+dayId+"_starttime").val('9:00 AM');
+  iBody.find("input#widget_day_"+dayId+"endtime").timepicker('setTime','5:30 PM')
+  iBody.find("input#widget_day_"+dayId+"_endtime").val('5:30 PM');
   iBody.find("button#widget_day_"+dayId+"_btnsaveadd")[0].click();
 }
 
